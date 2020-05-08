877690fbdb6ee9891c18c0d93d689df80c40f05d
Gero Mendy <geromendy@gmail.com>
2020-05-08 10:29:55 -0300

    iniciado scketch y agregado menu escondido para nuevo viaje/plan

---
+++ Scketchs/main/behav.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded",function(){
+    "use strict";
+
+    console.log("Working -- 0");
+    
+
+    document.querySelector("#menu_usuario").addEventListener("click", function(){mostrar_hidden_menu(true,0)});
+    document.querySelector("#user_menu_cerrar").addEventListener("click",function(){mostrar_hidden_menu(false,0)});
+    
+    document.querySelector("#agregar_viaje").addEventListener("click", function(){mostrar_hidden_menu(true,1)});
+    document.querySelector("#new_menu_cerrar").addEventListener("click",function(){mostrar_hidden_menu(false,1)});
+    
+
+    function mostrar_hidden_menu(mostrar,id_menu){
+        console.log("mostrar_hidden_menu --- 0");
+        console.log("mostrar = " + mostrar);
+        
+        
+        let menu;
+        switch(id_menu){
+
+            
+            case 0: menu =  document.querySelector("#hidden-user-menu");
+            break;
+            case 1: menu = document.querySelector("#hidden-new-menu");
+            break;
+        }
+        console.log("mostrar_hidden_menu --- 1");
+        console.log(menu);
+        
+        if(mostrar){
+            console.log("mostrar_hidden_menu --- 2.display");
+
+            menu.classList.remove("hidden");
+        }else{
+
+            console.log("mostrar_hidden_menu --- 2.hidden");
+            menu.classList.add("hidden");
+
+        }
+        console.log("mostrar_hidden_menu --- 3");
+
+    }
+
+    mostrar_hidden_menu(false,0);
+    mostrar_hidden_menu(false,1);
+
+});
---
+++ Scketchs/main/readme.md
@@ -0,0 +1,4 @@
+# Scketch index TPE-Metodologias
+
+Este sería el scketch del index para la primer presentacion del TPE.
+Al ser solo para presentar graficamente durante la meet, la mayor parte del codigo esta 'hardcodeado'. Deberia presentarse usando el formato 'Galaxy S5' (360px * 640px) del modo Desarrollador Web de chrome (al que se ingresa presionando 'F12' o usando 'inspeccionar elemento' en el menu contextual).
