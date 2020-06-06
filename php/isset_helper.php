90b116c1c17eaf55f4718a86cc4281cc88c5f978
Gero Mendy <geromendy@gmail.com>
2020-06-06 16:24:16 -0300

    Tarea79 (Bug), Tarea61 - Agregado insertHotel en controller y corregido fallo en addHotel en model

---
+++ php/isset_helper.php
@@ -0,0 +1,20 @@
+<?php
+
+    //Revisa si el array pasado como parámetro esta seteado.
+    //[IMPORTANTE] para revisar el array $_POST, hay que enviar el parámetro $arr como 'Null'.
+    function validData($arr=null,$keys=null){
+        if($arr==null){
+            foreach($keys as $k){
+                if( !isset($_POST[$k]) || $_POST[$k]=='' ){
+                    return false;
+                }
+            }
+        }else{
+            foreach($keys as $k){
+                if( !isset($arr[$k]) || $arr[$k]=='' ){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
