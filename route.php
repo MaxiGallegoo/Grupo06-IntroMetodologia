cdedc5c32e50a4d2324478d5cafa92781b8c3708
MaxiGallegoo <maxi.doblem@gmail.com>
2020-06-29 23:05:48 -0300

    fix route

--- route.php
+++ route.php
@@ -5,7 +5,18 @@
     define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]));
 	
     $router = new Router();
-	$router->addRoute("mapa/:ID","GET","controller","verMapa");
+    $router->addRoute("mapa/:ID","GET","controller","verMapa");
+
+    $router->addRoute("hotel/nuevo","GET","controller","displayAddHotel");// (Todavía no implementado).
+    $router->addRoute("hotel/nuevo/:ID","POST","controller","insertHotel");
+    $router->addRoute("hotel/nuevo","POST","controller","insertHotel");
+    $router->addRoute("hotel/mail","POST","controller","leerMail");
+
+    /*
+    //Hardcode para testing.
+    $router->addRoute("hardcode_testing_addHotel","GET","controller","testingModel_addHotel");
+    */
+
     $router->setDefaultRoute("controller","index");
 	
     $router->route($_GET["action"], $_SERVER['REQUEST_METHOD']);
