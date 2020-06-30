c8c57e29d9634d662a65e9be11c64cf86a7ef8b4
MaxiGallegoo <maxi.doblem@gmail.com>
2020-06-29 23:05:17 -0300

    actualizo bbdd para testing

--- Testing/DB_para_testing.sql
+++ Testing/DB_para_testing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-05-2020 a las 14:19:15
+-- Tiempo de generación: 05-06-2020 a las 20:39:30
 -- Versión del servidor: 10.1.32-MariaDB
 -- Versión de PHP: 7.2.5
 
@@ -30,28 +30,30 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `plan` (
   `id_plan` int(11) NOT NULL,
-  `viaje_id_viaje` int(11) NOT NULL,
-  `tipo` int(11) NOT NULL,
+  `id_viaje` int(11) NOT NULL,
   `titulo` varchar(63) NOT NULL,
-  `descripcion` varchar(255) DEFAULT NULL
+  `descripcion` varchar(255) DEFAULT NULL,
+  `tipo` int(4) NOT NULL,
+  `latitud` int(11) DEFAULT NULL,
+  `longitud` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `plan`
 --
 
-INSERT INTO `plan` (`id_plan`, `viaje_id_viaje`, `tipo`, `titulo`, `descripcion`) VALUES
-(0, 0, 0, 'Vuelo Arg-Uru', 'Vuelo de Ida a Uruguay'),
-(1, 0, 0, 'Vuelo Uru-Arg', 'Vuelo de Vuelta desde Uruguay'),
-(2, 0, 1, 'Hotel PdE', '36 noches en PdE'),
-(3, 0, 2, 'Museo', 'Visitar el museo'),
-(4, 0, 2, 'Recital Ciro', 'Entradas reservadas para Ciro Y Los Persas 05-12'),
-(5, 1, 1, 'Hotel \"Lugar Careta\"', '3 Noches en Olavarria'),
-(6, 1, 2, 'Almuerzo de Negocios', 'Reserva en restaurant Gourmet 05-07'),
-(7, 2, 0, 'Vuelo Arg-Par', 'Vuelo de Ida a Paraguay'),
-(8, 2, 0, 'Vuelo Par-Arg', 'Vuelo de Vuelta desde Paraguay'),
-(9, 2, 1, 'Hotel Gran Paraguay', '9 Noches en Asunción'),
-(10, 2, 2, 'Almuerzo de Negocios', 'Reserva en restaurant \"Asuncion\" 18-07');
+INSERT INTO `plan` (`id_plan`, `id_viaje`, `titulo`, `descripcion`, `tipo`, `latitud`, `longitud`) VALUES
+(5, 0, 'Vuelo Arg-Uru', 'Vuelo de Ida a Uruguay', 0, NULL, NULL),
+(6, 0, 'Vuelo Uru-Arg', 'Vuelo de Vuelta desde Uruguay', 0, NULL, NULL),
+(7, 0, 'Hotel PdE', '36 noches en PdE', 1, NULL, NULL),
+(8, 0, 'Museo', 'Visitar el museo', 2, NULL, NULL),
+(9, 0, 'Recital Ciro', 'Entradas reservadas para Ciro Y Los Persas 05-12', 2, NULL, NULL),
+(10, 1, 'Hotel \"Lugar Careta\"', '3 Noches en Olavarria', 1, NULL, NULL),
+(11, 1, 'Almuerzo de Negocios', 'Reserva en restaurant Gourmet 05-07', 2, NULL, NULL),
+(12, 2, 'Vuelo Arg-Par', 'Vuelo de Ida a Paraguay', 0, NULL, NULL),
+(13, 2, 'Vuelo Par-Arg', 'Vuelo de Vuelta desde Paraguay', 0, NULL, NULL),
+(14, 2, 'Hotel Gran Paraguay', '9 Noches en Asunción', 1, NULL, NULL),
+(15, 2, 'Almuerzo de Negocios', 'Reserva en restaurant \"Asuncion\" 18-07', 2, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -60,19 +62,25 @@ INSERT INTO `plan` (`id_plan`, `viaje_id_viaje`, `tipo`, `titulo`, `descripcion`
 --
 
 CREATE TABLE `plan_hotel` (
-  `plan_id_plan` int(11) NOT NULL,
-  `nombre_hotel` varchar(63) DEFAULT NULL,
-  `dir_hotel` varchar(255) DEFAULT NULL
+  `id_plan` int(11) NOT NULL,
+  `nombre_hotel` varchar(255) NOT NULL,
+  `dir_hotel` varchar(255) NOT NULL,
+  `cant_personas` int(10) NOT NULL,
+  `cant_habitaciones` int(10) NOT NULL,
+  `tipo_habitaciones` varchar(255) DEFAULT NULL,
+  `servicios` varchar(8091) DEFAULT NULL,
+  `fecha_in` date NOT NULL,
+  `fecha_out` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `plan_hotel`
 --
 
-INSERT INTO `plan_hotel` (`plan_id_plan`, `nombre_hotel`, `dir_hotel`) VALUES
-(2, 'PdE International Resort', 'Central 1450'),
-(5, 'Lugar Careta', 'Gral. Roca 570'),
-(9, 'Gran Paraguay', 'Roque Santa Cruz 223');
+INSERT INTO `plan_hotel` (`id_plan`, `nombre_hotel`, `dir_hotel`, `cant_personas`, `cant_habitaciones`, `tipo_habitaciones`, `servicios`, `fecha_in`, `fecha_out`) VALUES
+(7, 'PdE International Resort', 'Central 1450', 0, 0, '', '', '0000-00-00', '0000-00-00'),
+(10, 'Lugar Careta', 'Gral. Roca 570', 0, 0, '', '', '0000-00-00', '0000-00-00'),
+(14, 'Gran Paraguay', 'Roque Santa Cruz 223', 0, 0, '', '', '0000-00-00', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -81,9 +89,7 @@ INSERT INTO `plan_hotel` (`plan_id_plan`, `nombre_hotel`, `dir_hotel`) VALUES
 --
 
 CREATE TABLE `plan_otro` (
-  `plan_id_plan` int(11) NOT NULL,
-  `titulo` varchar(63) NOT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
+  `id_plan` int(11) NOT NULL,
   `direccion` varchar(255) DEFAULT NULL,
   `fecha` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -92,11 +98,11 @@ CREATE TABLE `plan_otro` (
 -- Volcado de datos para la tabla `plan_otro`
 --
 
-INSERT INTO `plan_otro` (`plan_id_plan`, `titulo`, `descripcion`, `direccion`, `fecha`) VALUES
-(3, 'Museo', NULL, 'Calle 5 256', NULL),
-(4, 'Ciro Y Los Persas', 'Entradas Reservadas para el recital', 'Playa \"Farándula\"', '2020-12-05'),
-(6, 'Almuerzo de negocios', 'Reserva en restaurant \"Gourmet\"', 'Rivadavia 980', '2020-07-05'),
-(10, 'Almuerzo de Negocios', 'Reserva en restaurant \"Asunción\"', 'C.A. López 780', '2020-08-17');
+INSERT INTO `plan_otro` (`id_plan`, `direccion`, `fecha`) VALUES
+(8, 'Calle 5 256', NULL),
+(9, 'Playa \"Farándula\"', '2020-12-05'),
+(11, 'Rivadavia 980', '2020-07-05'),
+(15, 'C.A. López 780', '2020-08-17');
 
 -- --------------------------------------------------------
 
@@ -105,7 +111,7 @@ INSERT INTO `plan_otro` (`plan_id_plan`, `titulo`, `descripcion`, `direccion`, `
 --
 
 CREATE TABLE `plan_vuelo` (
-  `plan_id_plan` int(11) NOT NULL,
+  `id_plan` int(11) NOT NULL,
   `aeropuerto_salida` varchar(255) NOT NULL,
   `aeropuerto_llegada` varchar(255) NOT NULL,
   `cant_escalas` int(11) NOT NULL
@@ -115,11 +121,11 @@ CREATE TABLE `plan_vuelo` (
 -- Volcado de datos para la tabla `plan_vuelo`
 --
 
-INSERT INTO `plan_vuelo` (`plan_id_plan`, `aeropuerto_salida`, `aeropuerto_llegada`, `cant_escalas`) VALUES
-(0, 'Ezeiza', 'Interncaional de Punta del Este', 0),
-(1, 'Interncaional de Punta del Este', 'Ezeiza', 0),
-(7, 'Ezeiza', 'Asuncion', 1),
-(8, 'Asuncion', 'Ezeiza', 0);
+INSERT INTO `plan_vuelo` (`id_plan`, `aeropuerto_salida`, `aeropuerto_llegada`, `cant_escalas`) VALUES
+(5, 'Ezeiza', 'Interncaional de Punta del Este', 0),
+(6, 'Interncaional de Punta del Este', 'Ezeiza', 0),
+(12, 'Ezeiza', 'Asuncion', 1),
+(13, 'Asuncion', 'Ezeiza', 0);
 
 -- --------------------------------------------------------
 
@@ -130,8 +136,8 @@ INSERT INTO `plan_vuelo` (`plan_id_plan`, `aeropuerto_salida`, `aeropuerto_llega
 CREATE TABLE `viaje` (
   `id_viaje` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL,
-  `cod_pais_origen` varchar(3) NOT NULL,
-  `cod_pais_destino` varchar(3) NOT NULL,
+  `cod_pais_origen` varchar(4) NOT NULL,
+  `cod_pais_destino` varchar(4) NOT NULL,
   `fecha_inicio` date NOT NULL,
   `fecha_fin` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -141,9 +147,9 @@ CREATE TABLE `viaje` (
 --
 
 INSERT INTO `viaje` (`id_viaje`, `nombre`, `cod_pais_origen`, `cod_pais_destino`, `fecha_inicio`, `fecha_fin`) VALUES
-(0, 'Vacaciones - Punta del Este', 'ARG', 'URU', '2020-11-27', '2021-01-02'),
-(1, 'Negocios - Olavarria', 'ARG', 'ARG', '2020-06-03', '2020-06-07'),
-(2, 'Negocios - Asuncion', 'ARG', 'PAR', '2020-07-15', '2020-07-25');
+(7, 'Vacaciones - Punta del Este', 'ARG', 'URU', '2020-11-27', '2021-01-02'),
+(8, 'Negocios - Olavarria', 'ARG', 'ARG', '2020-06-03', '2020-06-07'),
+(9, 'Negocios - Asuncion', 'ARG', 'PAR', '2020-07-15', '2020-07-25');
 
 -- --------------------------------------------------------
 
@@ -152,7 +158,7 @@ INSERT INTO `viaje` (`id_viaje`, `nombre`, `cod_pais_origen`, `cod_pais_destino`
 --
 
 CREATE TABLE `viaje_estadisticas` (
-  `viaje_id_viaje` int(11) NOT NULL,
+  `id_viaje` int(11) NOT NULL,
   `id_estadisticas` int(11) NOT NULL,
   `km_recorridos` int(11) NOT NULL,
   `cant_paises_visitados` int(11) NOT NULL,
@@ -164,10 +170,10 @@ CREATE TABLE `viaje_estadisticas` (
 -- Volcado de datos para la tabla `viaje_estadisticas`
 --
 
-INSERT INTO `viaje_estadisticas` (`viaje_id_viaje`, `id_estadisticas`, `km_recorridos`, `cant_paises_visitados`, `huella_carbono`, `horas_viajadas`) VALUES
-(0, 0, 612, 1, '27.00', '1.82'),
-(1, 1, 707, 0, '15.90', '6.28'),
-(2, 2, 2246, 1, '98.59', '3.87');
+INSERT INTO `viaje_estadisticas` (`id_viaje`, `id_estadisticas`, `km_recorridos`, `cant_paises_visitados`, `huella_carbono`, `horas_viajadas`) VALUES
+(7, 7, 612, 1, '27.00', '1.82'),
+(8, 8, 707, 0, '15.90', '6.28'),
+(9, 9, 2246, 1, '98.59', '3.87');
 
 --
 -- Índices para tablas volcadas
@@ -177,26 +183,13 @@ INSERT INTO `viaje_estadisticas` (`viaje_id_viaje`, `id_estadisticas`, `km_recor
 -- Indices de la tabla `plan`
 --
 ALTER TABLE `plan`
-  ADD PRIMARY KEY (`id_plan`,`viaje_id_viaje`),
-  ADD KEY `plan_viaje` (`viaje_id_viaje`);
+  ADD PRIMARY KEY (`id_plan`);
 
 --
 -- Indices de la tabla `plan_hotel`
 --
 ALTER TABLE `plan_hotel`
-  ADD PRIMARY KEY (`plan_id_plan`);
-
---
--- Indices de la tabla `plan_otro`
---
-ALTER TABLE `plan_otro`
-  ADD PRIMARY KEY (`plan_id_plan`);
-
---
--- Indices de la tabla `plan_vuelo`
---
-ALTER TABLE `plan_vuelo`
-  ADD PRIMARY KEY (`plan_id_plan`);
+  ADD KEY `id_plan` (`id_plan`);
 
 --
 -- Indices de la tabla `viaje`
@@ -208,41 +201,29 @@ ALTER TABLE `viaje`
 -- Indices de la tabla `viaje_estadisticas`
 --
 ALTER TABLE `viaje_estadisticas`
-  ADD PRIMARY KEY (`viaje_id_viaje`,`id_estadisticas`);
+  ADD PRIMARY KEY (`id_estadisticas`);
 
 --
--- Restricciones para tablas volcadas
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- Filtros para la tabla `plan`
+-- AUTO_INCREMENT de la tabla `plan`
 --
 ALTER TABLE `plan`
-  ADD CONSTRAINT `plan_viaje` FOREIGN KEY (`viaje_id_viaje`) REFERENCES `viaje` (`id_viaje`);
+  MODIFY `id_plan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
--- Filtros para la tabla `plan_hotel`
+-- AUTO_INCREMENT de la tabla `viaje`
 --
-ALTER TABLE `plan_hotel`
-  ADD CONSTRAINT `plan_hotel_plan` FOREIGN KEY (`plan_id_plan`) REFERENCES `plan` (`id_plan`);
+ALTER TABLE `viaje`
+  MODIFY `id_viaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- Filtros para la tabla `plan_otro`
---
-ALTER TABLE `plan_otro`
-  ADD CONSTRAINT `plan_otro_plan` FOREIGN KEY (`plan_id_plan`) REFERENCES `plan` (`id_plan`);
-
---
--- Filtros para la tabla `plan_vuelo`
---
-ALTER TABLE `plan_vuelo`
-  ADD CONSTRAINT `plan_vuelo_plan` FOREIGN KEY (`plan_id_plan`) REFERENCES `plan` (`id_plan`);
-
---
--- Filtros para la tabla `viaje_estadisticas`
+-- AUTO_INCREMENT de la tabla `viaje_estadisticas`
 --
 ALTER TABLE `viaje_estadisticas`
-  ADD CONSTRAINT `viaje_estadisticas_viaje` FOREIGN KEY (`viaje_id_viaje`) REFERENCES `viaje` (`id_viaje`);
+  MODIFY `id_estadisticas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
